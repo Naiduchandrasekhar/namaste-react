b8a1ae8e1493c0ba7641c3712db5b1dd19d2ba4a
Chandra Sekhar <naiduchandrasekhar10@gmail.com>
2024-01-21 21:00:28 +0530

    episode-01

---
+++ index.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("div", {id:'parent'}, 
+React.createElement("div", {id:'child'}, 
+React.createElement("h1", {id:'heading'}, "Hello World From React")))
+console.log(heading)
+
+const root = ReactDOM.createRoot(document.getElementById("#root"))
+root.render(heading)
