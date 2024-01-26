a496d1db6c8cd19ef99fc08af8b8c27c376f3c0b
Chandra Sekhar <naiduchandrasekhar10@gmail.com>
2024-01-26 16:57:32 +0530

    The Inception 01

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Namaste React 🚀
+
+# Chapter 01 - Inception
+
+# Theory -
+● What is Emmet?
+● Difference between a Library and Framework?
+● What is CDN? Why do we use it?
+● Why is React known as React?
+● What is crossorigin in script tag?
+● What is diference between React and ReactDOM
+● What is difference between react.development.js and react.production.js files via CDN?
+● What is async and defer? - see my Youtube video ;)
+Coding -
+● Set up all the tools in your laptop
+○ VS Code
+○ Chrome
+○ Extensions of Chrome
+● Create a new Git repo
+● Build your first Hello World program using,
+○ Using just HTML
+○ Using JS to manipulate the DOM
+○ Using React
+■ use CDN Links
+■ Create an Element
+■ Create nested React Elements
+■ Use root.render
+● Push code to Github (Theory as well as code)
+● Learn about Arrow Functions before the next class
+
--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-const heading = React.createElement("div", {id:'parent'}, 
-React.createElement("div", {id:'child'}, 
-React.createElement("h1", {id:'heading'}, "Hello World From React")))
-console.log(heading)
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("#root"))
-root.render(heading)
+// Babel is a transpiler where it converts into standard ECMAScript code into a backwards compatible version of JavaScript in current and older browsers 
+
+// JSX => React.createElement => ReactElement - JS Object => HTMLElement(render)
+
+const heading = React.createElement("h1" , {id:"heading"}, "Namaste React 🚀 from React Core syntax");
+
+const jsxHeading = <h1 id="heading">Namaste React 🚀 from JSX syntax</h1>
+
+console.log(heading);
+console.log(jsxHeading);
+
+//Component Compostion:
+//Component Composition refers to the process of combining smaller, reusable components together to create larger, more complex components. 
+//In other words, it involves building an application by breaking it down into smaller, more manageable parts and assembling them together like building blocks.
+
+const Title = () => (
+    <h1 className="head">Namaste React using JSX 🚀</h1>
+)
+
+const Heading  = () => (
+    <div id="container">
+        <Title />
+        {Title()}
+        <Title></Title>
+        <h2>By Akshay Saini</h2>
+    </div>
+)
+
+const root = ReactDOM.createRoot(document.getElementById("#root"));
+root.render(<Heading />);
